4d926ecba343251f1c56348eda4627812465148a
foold0g <papai.pappu@gmail.com>
2018-09-30 04:03:25 +0100

    Adding Copyright

--- src/components/coffeeclub/coffeeclub.jsx
+++ src/components/coffeeclub/coffeeclub.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../../components/header/header";
 import CoffeeTable from "../../components/coffeetable/coffeetable";
+import CopyRight from "../../components/copyright/copyright";
 
 class CoffeeClub extends Component {
   state = {};
@@ -10,6 +11,7 @@ class CoffeeClub extends Component {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Header />
         <CoffeeTable />
+        <CopyRight />
       </div>
     );
   }
---
+++ src/components/copyright/copyright.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class CopyRight extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="copyright">2018 © Suman Maparu. All rights reserved.</div>
+    );
+  }
+}
+
+export default CopyRight;
